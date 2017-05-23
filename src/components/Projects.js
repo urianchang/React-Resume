4fe9afde729cf6801b363e470307e9eef2d89c90
Urian Chang <urianchang@gmail.com>
2017-05-23 11:31:06 -0700

    Added Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import moment from 'moment';
 
 const Projects = props => {
-    // const getEducation = props.educationData.map(function(item, index) {
- //  		const startdate = moment(item.startDate).format('MMM, YYYY');
- //  		const enddate = moment(item.endDate).format('MMM, YYYY');
- //  		return (
-    //       <div key={index}>
-    //         <h3 className="text-italicize">{item.studyType}</h3>
- //  				  <h4>{item.institution}</h4>
- //  				  <p className="workDates">{startdate} - {enddate}</p>
- //  				</div>
-    //     )
- //  	});
-    //
+    const getProjects = props.projectsData.map(function(item, index) {
+  		return (
+          <div key={index}>
+            <h3 className="text-italicize">{item.name}</h3>
+            <span className='label label-primary'>{item.language}</span>
+  			<p>{item.description}</p>
+            <p></p>
+  		  </div>
+        )
+  	});
+
   	return (
   	  <section className="projects">
         <h2 className="text-uppercase"><i className="fa fa-lg fa-thumb-tack"></i> Projects</h2>
+        {getProjects}
       </section>
   	)
 };
